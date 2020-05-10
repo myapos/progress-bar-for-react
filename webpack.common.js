bf9460406ce2568589e47ccf55ee9815bb5d9d75
Myron Apostolakis <myron.apostolakis@sunlight.io>
2020-05-10 13:19:36 +0300

    packages update

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,9 @@
 const webpack = require("webpack");
 const path = require("path");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: {
