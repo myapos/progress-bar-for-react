ded468e49a603cba905ff6f21435c5c25d68d481
Myron Apostolakis <myron.apostolakis@sunlight.io>
2021-12-23 14:24:19 +0200

    coverage report

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -24,13 +24,5 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 
-  // const options = {
-  //   // send in the options from your webpack.config.js, so it works the same
-  //   // as your app's code
-  //   webpackOptions: require('../../webpack.dev.js'),
-  //   watchOptions: {},
-  // };
-
-  // on('file:preprocessor', webpackPreprocessor(options));
   return config;
 };
