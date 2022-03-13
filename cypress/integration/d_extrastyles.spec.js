936f51ba7428ca801216a5e7d9d79f364d126c50
Myron Apostolakis <myron.apostolakis@sunlight.io>
2022-03-13 21:08:24 +0200

    added extra styles with cypress and storybook

--- cypress/integration/d_extrastyles.spec.js
+++ cypress/integration/d_extrastyles.spec.js
@@ -4,20 +4,29 @@ function iget(doc, selector) {
 // https://medium.com/@mtiller/testing-react-components-using-storybook-and-cypress-1689a27f55aa
 describe('Extrastyles', () => {
   before(() => {
-    cy.viewport(1200, 600);
-    cy.visit('http://localhost:6006');
+    // Visit the storybook iframe page once per file
+    cy.visitStorybook();
+  });
+
+  // Note the use of `beforeEach`
+  beforeEach(() => {
+    // The first parameter is the category. This is the `title` in CSF or the value in `storiesOf`
+    // The second parameter is the name of the story. This is the name of the function in CSF or the value in the `add`
+    // This does not refresh the page, but will unmount any previous story and use the Storybook Router API to render a fresh new story
+    cy.loadStory('ProgressBar', 'extra_styles');
   });
 
   it('extra filler css', () => {
-    cy.get('#ExtraStyles #container > div > div > div').then((div) => {
-      const element = div[0];
+    cy.get("[class*='progressBarContainer'] > div > div")
+      .should('exist')
+      .then(([element, ...rest]) => {
+        const className = element.className;
 
-      const className = element.className;
+        const styles = window.getComputedStyle(element, null);
+        const backgroundColor = styles.getPropertyValue('background-color');
 
-      const styles = window.getComputedStyle(element, null);
-      const backgroundColor = styles.getPropertyValue('background-color');
-      // test css styling
-      expect(backgroundColor).to.include('rgb(255, 0, 0)');
-    });
+        // test css styling
+        expect(backgroundColor).to.include('rgb(0, 0, 255)');
+      });
   });
 });
