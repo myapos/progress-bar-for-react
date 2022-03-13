d7f49f52ea59a38790e7952d70e8ba3c61d5bfe6
Myron Apostolakis <myron.apostolakis@sunlight.io>
2022-03-13 22:45:10 +0200

    readmeclear

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -13,6 +13,7 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 import 'cypress-storybook/cypress';
+import '@cypress/code-coverage/support';
 
 // setGlobalConfig(sbPreview);
 
