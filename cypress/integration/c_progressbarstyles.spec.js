1b1a710e749e86f8b379d63425a70f2f406c040e
Myron Apostolakis <myron.apostolakis@sunlight.io>
2022-03-13 21:03:04 +0200

    added progress bar styles with cypress and storybook

--- cypress/integration/c_progressbarstyles.spec.js
+++ cypress/integration/c_progressbarstyles.spec.js
@@ -1,35 +1,43 @@
 describe('Progress bar styles', () => {
   before(() => {
-    cy.viewport(1200, 600);
-    cy.visit('http://localhost:6006');
+    // Visit the storybook iframe page once per file
+    cy.visitStorybook();
+  });
+
+  // Note the use of `beforeEach`
+  beforeEach(() => {
+    // The first parameter is the category. This is the `title` in CSF or the value in `storiesOf`
+    // The second parameter is the name of the story. This is the name of the function in CSF or the value in the `add`
+    // This does not refresh the page, but will unmount any previous story and use the Storybook Router API to render a fresh new story
+    cy.loadStory('ProgressBar', 'progress_bar_styles');
   });
 
   it('progress bar container filler css', () => {
-    cy.get('#ProgressBarStyles #container > div > div > div').then((div) => {
-      const element = div[0];
+    cy.get("[class*='progressBarContainer'] > div > div")
+      .should('exist')
+      .then(([element, ...rest]) => {
+        const styles = window.getComputedStyle(element, null);
+        const backgroundColor = styles.getPropertyValue('background-color');
+        // test css styling
+        expect(backgroundColor).to.include('rgb(0, 0, 255)');
+      });
 
-      const styles = window.getComputedStyle(element, null);
-      const backgroundColor = styles.getPropertyValue('background-color');
-      // test css styling
-      expect(backgroundColor).to.include('rgb(0, 0, 255)');
-    });
+    cy.get("[class*='progressBarContainer'] > div")
+      .should('exist')
+      .then(([element, ...rest]) => {
+        const styles = window.getComputedStyle(element, null);
+        const border = styles.getPropertyValue('border');
+        const borderRadius = styles.getPropertyValue('border-radius');
+        const width = styles.getPropertyValue('width');
+        const height = styles.getPropertyValue('height');
+        const position = styles.getPropertyValue('position');
 
-    cy.get('#ProgressBarStyles #container > div > div').then((div) => {
-      const element = div[0];
-
-      const styles = window.getComputedStyle(element, null);
-      const border = styles.getPropertyValue('border');
-      const borderRadius = styles.getPropertyValue('border-radius');
-      const width = styles.getPropertyValue('width');
-      const height = styles.getPropertyValue('height');
-      const position = styles.getPropertyValue('position');
-
-      // test css styling
-      expect(border).to.include('1px solid rgb(0, 0, 255)');
-      expect(borderRadius).to.include('1px');
-      expect(width).to.include('200px');
-      expect(height).to.include('50px');
-      expect(position).to.include('relative');
-    });
+        // test css styling
+        expect(border).to.include('1px solid rgb(0, 0, 255)');
+        expect(borderRadius).to.include('1px');
+        expect(width).to.include('200px');
+        expect(height).to.include('50px');
+        expect(position).to.include('relative');
+      });
   });
 });
